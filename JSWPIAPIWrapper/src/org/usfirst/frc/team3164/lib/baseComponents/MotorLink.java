a693ecd0b7b0bc41dfae1baf96595dd32f0c30be
Jaxon <jaxon007@verizon.net>
2015-01-17 13:27:47 -0500

    Added Drivetrain wrapper

---
+++ JSWPIAPIWrapper/src/org/usfirst/frc/team3164/lib/baseComponents/MotorLink.java
@@ -0,0 +1,98 @@
+package org.usfirst.frc.team3164.lib.baseComponents;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.usfirst.frc.team3164.lib.baseComponents.motors.IMotor;
+
+public class MotorLink {
+	private ArrayList<IMotor> motors;
+	private double power;
+	
+	public MotorLink(IMotor... ms) {
+		motors.addAll(Arrays.asList(ms));
+		this.power = 0;
+	}
+	
+	public ArrayList<IMotor> getMotors() {
+		return motors;
+	}
+	
+	/**
+	 * Gets current power
+	 * @return double power of the motors
+	 */
+	public double getPower() {
+		return power;
+	}
+	
+	/**
+	 * Updates power on all motors.
+	 */
+	public void updateAllMotors() {
+		for(IMotor m : motors) {
+			m.setPower(this.power);
+		}
+	}
+	
+	/**
+     * Set the power of the motor
+     * @param pwr power of the motor, -1.0 to 1.0
+     */
+    public void setPower(double pwr) {
+    	this.power = pwr;
+    	this.checkPower();
+        for(IMotor m : motors) {
+        	m.setPower(pwr);
+        }
+    }
+    
+    /**
+     * Set the scaled power of the motor
+     * @param pwr power of the motor, -100 to 100
+     */
+    public void setScaledPower(int pwr) {
+    	this.power = pwr;
+    	this.checkPower();
+        for(IMotor m : motors) {
+        	m.setScaledPower(pwr);
+        }
+    }
+    
+    /**
+     * Adds power for incrementing power purposes
+     * @param pwr power to increase/decrease by.
+     */
+    public void addPower(double pwr) {
+    	this.power+=pwr;
+    	this.checkPower();
+    	for(IMotor m : motors) {
+    		m.setPower(power);
+    	}
+    }
+    
+    /**
+     * Stop the motor.
+     */
+    public void stop() {
+        for(IMotor m : motors) {
+        	m.stop();
+        }
+    }
+    
+    @Deprecated //Unfinished
+    public void slowStop() {
+        for(IMotor m : motors) {
+        	m.slowStop();
+        }
+    }
+    
+    private void checkPower() {
+    	if(this.power>1.0) {
+        	this.power = 1.0;
+        }
+        if(this.power<-1.0) {
+        	this.power = -1.0;
+        }
+    }
+}
