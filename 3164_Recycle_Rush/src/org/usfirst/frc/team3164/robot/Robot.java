59cfed707b88d1d1f184d14db3843b2971e86b3a
Brendan Gregos <bgregos@gmail.com>
2015-01-19 13:22:33 -0500

    Added port assignment redirect and gyro reset button. Gyro also resets when robot is placed into test mode and upon first robot boot.

--- 3164_Recycle_Rush/src/org/usfirst/frc/team3164/robot/Robot.java
+++ 3164_Recycle_Rush/src/org/usfirst/frc/team3164/robot/Robot.java
@@ -1,12 +1,16 @@
-//This code was written for FRC Team 3164 by Jaxon Brown and Brendan Gregos!!
+//This code was written for FRC Team 3164 by Jaxon Brown and Brendan Gregos.
+//For an list of port assignments, please refer to the Wiki on Github.
 
 package org.usfirst.frc.team3164.robot;
+
+import org.usfirst.frc.team3164.lib.baseComponents.motors.IMotor;
+import org.usfirst.frc.team3164.lib.baseComponents.motors.JagMotor;
+import org.usfirst.frc.team3164.lib.robot.FRC2015.DriveTrain;
+import org.usfirst.frc.team3164.lib.robot.FRC2015.LiftMech;
 
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,9 +22,10 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 public class Robot extends IterativeRobot {
 	
 	//List of all declared robot parts
-	RobotDrive robotDrive;
+	DriveTrain robotDrive;
     Joystick stick1;
     Gyro drivegyro;
+    LiftMech guillotine;
 
     // Channels for the wheels
     final int frontLeftChannel	= 2;
@@ -34,12 +39,17 @@ public class Robot extends IterativeRobot {
     //Constructor
     public Robot() {
     	//Disable robot if no comms fail- prevent Charlie drivers!
-    	robotDrive.setExpiration(0.1);
+    	//robotDrive.setExpiration(0.1);
         //Setup new drivetrain
-    	robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
-    	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
-    	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);		// you may need to change or remove this to match your robot
-
+    	IMotor frontleft= new JagMotor(0,true);//reversed
+    	IMotor rearleft= new JagMotor(1,true);//reversed
+    	IMotor frontright= new JagMotor(2);
+    	IMotor rearright= new JagMotor(3);
+    	
+    	drivegyro=new Gyro(0);
+    	//guillotine=new LiftMech(, null, null, null);
+    	
+    	robotDrive = new DriveTrain(frontleft,frontright,rearleft,rearright,false);
         stick1 = new Joystick(joystickChannel);
     }
     
@@ -58,7 +68,6 @@ public class Robot extends IterativeRobot {
     public void autonomousInit() {
     	
     }
-    //Hello
     
     /**
      * This function is called periodically during autonomous
@@ -74,7 +83,11 @@ public class Robot extends IterativeRobot {
     @Override
     public void teleopPeriodic() {
     	
+    	////Wheel movement/////
     	robotDrive.mecanumDrive_Cartesian(stick1.getX(), stick1.getY(), stick1.getZ(), drivegyro.getAngle() );
+    	
+    	//emergency gyro reset during match
+    	if(stick1.getRawButton(10)){ drivegyro.initGyro(); }
     }
     
     /**
@@ -82,6 +95,7 @@ public class Robot extends IterativeRobot {
      */
     @Override
     public void testPeriodic() {
+    	drivegyro.initGyro(); //Reset Gyro when robot placed into test mode.
     	
     }
     
