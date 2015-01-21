d40c9079bed37d1921f73253ab28e0a703a55d27
Jaxon <jaxon007@verizon.net>
2015-01-21 18:36:05 -0500

    Pushed. Libs are ready for teleop

--- 3164_Recycle_Rush/src/org/usfirst/frc/team3164/lib/robot/FRC2015/JSRobot.java
+++ 3164_Recycle_Rush/src/org/usfirst/frc/team3164/lib/robot/FRC2015/JSRobot.java
@@ -1,7 +1,45 @@
 package org.usfirst.frc.team3164.lib.robot.FRC2015;
 
-public class JSRobot {
-	private DriveTrain driveTrain;
-	private LiftMech liftMech;
-	private PinchMech pinchMech;
+import org.usfirst.frc.team3164.lib.baseComponents.LimitSwitch;
+import org.usfirst.frc.team3164.lib.baseComponents.MotorEncoder;
+import org.usfirst.frc.team3164.lib.baseComponents.motors.JagMotor;
+import org.usfirst.frc.team3164.lib.baseComponents.motors.VicMotor;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public abstract class JSRobot extends IterativeRobot {
+	//Drive train
+	public static int DRIVETRAIN_MOTOR_FRONTLEFT = 0;
+	public static int DRIVETRAIN_MOTOR_FRONTRIGHT = 0;
+	public static int DRIVETRAIN_MOTOR_REARLEFT = 0;
+	public static int DRIVETRAIN_MOTOR_REARRIGHT = 0;
+	
+	//Lift Mech
+	public static int LIFTMECH_MOTOR_1 = 0;
+	public static int LIFTMECH_LIMIT_TOP = 0;
+	public static int LIFTMECH_LIMIT_BOTTOM = 0;
+	public static int LIFTMECH_ENCODER_AC = 0;
+	public static int LIFTMECH_ENCODER_BC = 0;
+	
+	//Pinch Mech
+	public static int PINCHMECH_MOTOR = 0;
+	public static int PINCHMECH_ENCODER_AC = 0;
+	public static int PINCHMECH_ENCODER_BC = 0;
+	
+	
+	
+	
+	public DriveTrain driveTrain;
+	public LiftMech liftMech;
+	public PinchMech pinchMech;
+	
+	public JSRobot() {
+		this.driveTrain = new DriveTrain(new JagMotor(JSRobot.DRIVETRAIN_MOTOR_FRONTLEFT),
+				new JagMotor(JSRobot.DRIVETRAIN_MOTOR_FRONTRIGHT), new JagMotor(JSRobot.DRIVETRAIN_MOTOR_REARLEFT),
+				new JagMotor(JSRobot.DRIVETRAIN_MOTOR_REARRIGHT), false);
+		this.liftMech = new LiftMech(new LimitSwitch(JSRobot.LIFTMECH_LIMIT_TOP), new LimitSwitch(JSRobot.LIFTMECH_LIMIT_BOTTOM),
+				new MotorEncoder(JSRobot.LIFTMECH_ENCODER_AC, JSRobot.LIFTMECH_ENCODER_BC, false), new VicMotor(JSRobot.LIFTMECH_MOTOR_1));
+		this.pinchMech = new PinchMech(new VicMotor(JSRobot.PINCHMECH_MOTOR),
+				new MotorEncoder(JSRobot.PINCHMECH_ENCODER_AC, JSRobot.PINCHMECH_ENCODER_BC, false));
+	}
 }
--- 3164_Recycle_Rush/src/org/usfirst/frc/team3164/robot/Robot.java
+++ 3164_Recycle_Rush/src/org/usfirst/frc/team3164/robot/Robot.java
@@ -7,6 +7,7 @@ import org.usfirst.frc.team3164.lib.baseComponents.Watchcat;
 import org.usfirst.frc.team3164.lib.baseComponents.motors.IMotor; 
 import org.usfirst.frc.team3164.lib.baseComponents.motors.JagMotor;
 import org.usfirst.frc.team3164.lib.robot.FRC2015.DriveTrain;
+import org.usfirst.frc.team3164.lib.robot.FRC2015.JSRobot;
 import org.usfirst.frc.team3164.lib.robot.FRC2015.LiftMech;
 
 import edu.wpi.first.wpilibj.Gyro;
@@ -20,19 +21,11 @@ import edu.wpi.first.wpilibj.Joystick;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot {
+public class Robot extends JSRobot {
 	
 	//List of all declared robot parts
-	DriveTrain robotDrive;
     Joystick stick1;
     Gyro drivegyro;
-    LiftMech guillotine;
-
-    // Channels for the wheels
-    final int frontLeftChannel	= 2;
-    final int rearLeftChannel	= 3;
-    final int frontRightChannel	= 1;
-    final int rearRightChannel	= 0;
     
     // The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
@@ -42,15 +35,7 @@ public class Robot extends IterativeRobot {
     	//Disable robot if no comms fail- prevent Charlie drivers!
     	Watchcat.init();
         //Setup new drivetrain
-    	IMotor frontleft= new JagMotor(0,true);//reversed
-    	IMotor rearleft= new JagMotor(1,true);//reversed
-    	IMotor frontright= new JagMotor(2);
-    	IMotor rearright= new JagMotor(3);
-    	
     	drivegyro=new Gyro(0);
-    	//guillotine=new LiftMech(, null, null, null);
-    	
-    	robotDrive = new DriveTrain(frontleft,frontright,rearleft,rearright,false);
         stick1 = new Joystick(joystickChannel);
     }
     
@@ -85,7 +70,7 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
     	
     	////Wheel movement/////
-    	robotDrive.mecanumDrive_Cartesian(stick1.getX(), stick1.getY(), stick1.getZ(), drivegyro.getAngle() );
+    	driveTrain.mecanumDrive_Cartesian(stick1.getX(), stick1.getY(), stick1.getZ(), drivegyro.getAngle() );
     	
     	//emergency gyro reset during match
     	if(stick1.getRawButton(10)){ drivegyro.initGyro(); }
