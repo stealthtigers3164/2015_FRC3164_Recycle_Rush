e36dc05c9194f5409970178ae844cfb0016fdf47
Jaxon <jaxon007@verizon.net>
2015-02-03 16:37:46 -0500

    Thread for detecting objects added

--- 3164_Recycle_Rush/src/org/usfirst/frc/team3164/lib/baseComponents/sensors/NXTRangefinder.java
+++ 3164_Recycle_Rush/src/org/usfirst/frc/team3164/lib/baseComponents/sensors/NXTRangefinder.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3164.lib.baseComponents.sensors;
 
+import org.usfirst.frc.team3164.lib.util.ICallback;
+
 import edu.wpi.first.wpilibj.AnalogInput;
 
 /**
@@ -23,5 +25,51 @@ public class NXTRangefinder {
 	public int getRange(){
 		int range=(int) (rangefinder.getVoltage()*100); //Rangefinder sends values 0-254 as 0-2.54 volts.
 		return range;
+	}
+	
+	private ObjLTH objLTH;
+	private class ObjLTH extends Thread {
+		private int lastRange;
+		private int range;
+		private boolean go = true;
+		private ICallback cb;
+		public ObjLTH(ICallback cb) {
+			this.cb = cb;
+		}
+		public void kill() {
+			go = false;
+		}
+		@Override
+		public void run() {
+			range = getRange();
+			lastRange = range;
+			while(go) {
+				range = getRange();
+				if(Math.abs(range-lastRange)>40 || range<50) {//TODO Thresholds to be changed as needed
+					break;
+				}
+				lastRange = range;
+				try {Thread.sleep(30);} catch(Exception ex) {}
+			}
+			cb.call();
+		}
+	}
+	
+	/**
+	 * Starts a listener that will execute the callback when an object is located.
+	 * @param cb Callback to be run when an object is located.
+	 */
+	public void startObjectListener(ICallback cb) {
+		this.objLTH = new ObjLTH(cb);
+		this.objLTH.start();
+	}
+	
+	/**
+	 * Stops the object listener.
+	 */
+	public void stopObjectListener() {
+		if(objLTH!=null && objLTH.isAlive()) {
+			objLTH.kill();
+		}
 	}
 }
