db122c130da34cc09a8fc87ef41875917b646384
Brendan Gregos <bgregos@gmail.com>
2015-02-14 10:45:22 -0500

    Moved autonomous to separate class.

--- 3164_Recycle_Rush/src/org/usfirst/frc/team3164/robot/Robot.java
+++ 3164_Recycle_Rush/src/org/usfirst/frc/team3164/robot/Robot.java
@@ -17,6 +17,7 @@ import org.usfirst.frc.team3164.lib.vision.ToteFinder;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -41,6 +42,7 @@ public class Robot extends JSRobot {
     
     //Constructor
     public Robot() {
+    	SmartDashboard.putBoolean("IsCompCode", true);
     	rbt = this;
         //Setup new drivetrain
     	driveGyro = new Gyro(0);
@@ -64,111 +66,8 @@ public class Robot extends JSRobot {
      */
     @Override
     public void autonomousInit() {
-    	driveTrain.startDrive(0.2, DriveDir.LEFT, driveGyro);//Begins to drive left
-    	ToteFinder tfind = new ToteFinder(new ICallback() {//Starts listening for tote
-    		@Override
-    		public void call() {//Tote has been found!
-    			Robot.rbt.auto_hasFound = true;//Set cb var to true
-    		}
-    	});
-    	while(!auto_hasFound) {Timer.waitMillis(10);}//Waits for the callback
-    	driveTrain.stop();//Stop the robot
+    	//autonomous is now found in the Autonomous class.
     }
-    /*@Override
-    public void autonomousInit() {
-    	driveTrain.turn(180, TurnDir.LEFT, driveGyro);//Turn 180 to face the totes
-    	driveTrain.driveTime(1.0, DriveDir.FORWARDS, 3000, driveGyro);//Drives forwards towards the tote
-    	pinchMech.close();//Closes the pincher to pick up first tote
-    	Timer.waitSec(2);
-    	liftMech.startGoingUpToPreset();//Begins raising the lift
-    	Timer.waitSec(1);
-    	driveTrain.driveTime(-1.0, DriveDir.REVERSE, 3000, driveGyro);//Reverses to previous location
-    	driveTrain.startDrive(1.0, DriveDir.LEFT, driveGyro);//Begins to drive left
-    	ToteFinder tfind = new ToteFinder(new ICallback() {//Starts listening for tote
-    		@Override
-    		public void call() {//Tote has been found!
-    			Robot.rbt.auto_hasFound = true;//Set cb var to true
-    		}
-    	});
-    	while(!auto_hasFound) {Timer.waitMillis(10);}//Waits for the callback
-    	driveTrain.stop();//Stop the robot
-    	this.auto_hasFound = false;//Reset the cb var
-    	
-    	driveTrain.driveTime(1.0, DriveDir.FORWARDS, 3000, driveGyro);//Drive forwards towards the tote
-    	pinchMech.open();//open the pinch mech; it should be at preset by now
-    	Timer.waitSec(2);
-    	liftMech.goDown();//lower pinch mech
-    	while(liftMech.isDown()) {//Wait for the pinch mech to lower all the way
-    		Timer.waitMillis(30);
-    	}
-    	pinchMech.close();//close the mech. 
-    	Timer.waitMillis(2);
-    	liftMech.startGoingUpToPreset();
-    	driveTrain.driveTime(-1.0, DriveDir.REVERSE, 3000, driveGyro);
-    	driveTrain.startDrive(1.0, DriveDir.LEFT, driveGyro);
-    	tfind = new ToteFinder(new ICallback() {
-    		@Override
-    		public void call() {
-    			Robot.rbt.auto_hasFound = true;
-    		}
-    	});
-    	while(!auto_hasFound) {}
-    	driveTrain.stop();
-    	this.auto_hasFound = false;
-    	
-    	driveTrain.driveTime(1.0, DriveDir.FORWARDS, 3000, driveGyro);
-    	pinchMech.open();
-    	Timer.waitSec(2);
-    	liftMech.goDown();
-    	while(liftMech.isDown()) {
-    		Timer.waitMillis(30);
-    	}
-    	pinchMech.close();
-    	Timer.waitMillis(2);
-    	liftMech.goUp();
-    	new Scheduler(1000, new ICallback() {
-    		@Override
-    		public void call() {
-    			Robot.rbt.liftMech.stop();
-    		}
-    	});
-    	driveTrain.driveTime(-1.0, DriveDir.REVERSE, 10000, driveGyro);
-    }*/
-   // @Override
-   // public void autonomousInit() {
-    	/*Accel1Test ac = new Accel1Test();
-    	driveTrain.startDrive(0.3, DriveDir.FORWARDS);
-    	while(true) {
-    		if(ac.getDistY()>=10) {
-    			break;
-    		}
-    	}
-    	driveTrain.stop();*/
-    	/*BuiltInAccelerometer acc = new BuiltInAccelerometer();
-    	double yOff = acc.getY();
-    	int counter = 0;
-    	System.out.println("Starting...");
-    	driveTrain.startDrive(1.0, DriveDir.FORWARDS);
-    	while(true) {
-    		System.out.println(acc.getY()-yOff);
-    		Timer.waitMilis(20);
-    		if(counter==50*3) {
-    			System.out.println("Stopping...");
-    			driveTrain.stop();
-    			Timer.waitMilis(1000);
-    			System.out.println("Reversing...");
-    			driveTrain.startDrive(1.0, DriveDir.REVERSE);
-    		}
-    		if(counter==50*3+50*3) {
-    			break;
-    		}
-    		counter++;
-    	}
-    	driveTrain.stop();
-    	System.out.println("Stopped.");*/
-    	//System.out.println("Ready...");
-    	//System.out.println(new I2CRFT1(0).getAngle());
-   // }
     
     /**
      * This function is called periodically during autonomous
